de7d9028759629b48fab40cfa483cc234a726481
GemmaGarcias <gemma.garcias.e@gmail.com>
2020-08-04 16:24:10 +0200

    Modify paragraph to list on properties

--- src/components/FilmsView/FilmsView.js
+++ src/components/FilmsView/FilmsView.js
@@ -21,15 +21,15 @@ function FilmsView() {
     <div className="Films-container"> 
         {isLoading ? <p className="Films-loading">Wait Im Loading comments for you</p> : <h1 className={"Films-title"}>FILMS</h1>}
         {films.results && films.results.map((film, i) => (
-            <div key={i} className="Films-items">
+            <section key={i} className="Films-items">
                 <h3>{film.title}</h3>
-                <div>
-                    <p><strong>Release date: </strong>{film.release_date}</p>
-                    <p><strong>Director: </strong>{film.director}</p>
-                    <p><strong>Producers: </strong>{film.producer}</p>
-                    <p><strong>Opening: </strong>{film.opening_crawl}</p>
-                </div>
-            </div>
+                <ul>
+                    <li><strong>Release date: </strong>{film.release_date}</li>
+                    <li><strong>Director: </strong>{film.director}</li>
+                    <li><strong>liroducers: </strong>{film.producer}</li>
+                    <li><strong>Oliening: </strong>{film.opening_crawl}</li>
+                </ul>
+            </section>
         ))}
     </div>
   );
