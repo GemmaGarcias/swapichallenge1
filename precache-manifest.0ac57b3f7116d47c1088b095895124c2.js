301325da0a1d843d452869e5a2d608bb8466cba4
GemmaGarcias <gemma.garcias.e@gmail.com>
2020-08-05 13:14:11 +0200

    Updates

---
+++ precache-manifest.0ac57b3f7116d47c1088b095895124c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a4e4abd0b141e85bfbfa578ab658293",
+    "url": "/swapichallenge1/index.html"
+  },
+  {
+    "revision": "5c02c4f09ad1be16969f",
+    "url": "/swapichallenge1/static/css/main.d70dcfa8.chunk.css"
+  },
+  {
+    "revision": "0c95b9dcddc15fce4b0e",
+    "url": "/swapichallenge1/static/js/2.282d0d99.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/swapichallenge1/static/js/2.282d0d99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c02c4f09ad1be16969f",
+    "url": "/swapichallenge1/static/js/main.0f7a37b2.chunk.js"
+  },
+  {
+    "revision": "09aa10f20459e62f44df",
+    "url": "/swapichallenge1/static/js/runtime-main.98885748.js"
+  },
+  {
+    "revision": "9bb4deef87f3a1c98914be941750631e",
+    "url": "/swapichallenge1/static/media/SwLogo.9bb4deef.png"
+  }
+]);
